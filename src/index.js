e3ea4a4f59adaef7e118e5abd5ff2c9df8f18ad6
Faiz Mohd <42231693+ifaizmohd@users.noreply.github.com>
2023-02-01 00:06:58 +0530

    added proxy for api authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import "babel-polyfill";
 import express from "express";
 import { matchRoutes } from "react-router-config";
+import proxy from "express-http-proxy";
 import Routes from "./client/Routes";
 import createStore from "./helpers/createStore";
 import renderer from "./helpers/renderer";
 
 const app = express();
+
+// Setting up proxy.
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.header["x-forwarded-host"] = "localhost:3000";
+      return opts;
+    },
+  })
+);
 const port = 3000;
 app.use(express.static("public"));
 

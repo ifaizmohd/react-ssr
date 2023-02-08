a9f2369baabadf84d720e6fa0bbcff47208bcd60
Faiz Mohd <42231693+ifaizmohd@users.noreply.github.com>
2023-02-09 00:00:29 +0530

    added correct response for not found pages

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-const NotFoundPage = () => <h1>Oops, route not found.</h1>;
+const NotFoundPage = ({ staticContext }) => {
+  staticContext.notFound = true;
+  return <h1>Oops, route not found.</h1>;
+};
 
 export default {
   component: NotFoundPage,
